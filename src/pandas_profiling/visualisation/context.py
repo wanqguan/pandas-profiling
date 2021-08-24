df2cc476d3bdf2c4e118d2d913a2eedece75680b
shiyunlai233 <shiyunlaichina@163.com>
2021-08-24 11:11:24 +0800

    zh plot font"

--- src/pandas_profiling/visualisation/context.py
+++ src/pandas_profiling/visualisation/context.py
@@ -38,6 +38,7 @@ def manage_matplotlib_context() -> Any:
         "image.cmap": "Greys",
         "font.family": ["sans-serif"],
         "font.sans-serif": [
+            "SimHei", # 我加的
             "Arial",
             "Liberation Sans",
             "Bitstream Vera Sans",
